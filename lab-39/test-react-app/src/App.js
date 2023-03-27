2615cb32dd186e9ce0bc9305b07623a3f92d4df3
Olena <7890572@ukr.net>
2023-03-27 21:00:28 +0200

    added

---
+++ lab-39/test-react-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './components/Header.js'; 
+import Navigation from './components/Navigation.js';
+import Container from './components/Container.js';
+
+
+
+
+function App() {
+    return(
+        <div className="App">
+            <Header></Header>
+            <Navigation></Navigation>
+            <Container></Container>
+            
+            
+            
+
+            
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ lab-39/test-react-app/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div className="nav">
+                <aside>
+                    <ul className='nav_ul'>
+                        <li><a href='#'>MTC</a></li>
+                        <li><a href='#'>KyivStar</a></li>
+                        <li><a href='#'>lifecell</a></li>
+                    </ul>
+                </aside>
+            </div>
+            );
+        }
+    }
+  
+  export default Navigation;
+  
